deb4e4c59bb6b07da6d58be0cd5ae39729c4fcfc
Fisichelaa <fisichela97@gmail.com>
2018-06-02 19:48:02 +0700

    Update Versi

---
+++ no1.js
@@ -0,0 +1,22 @@
+function Tampilkan(){
+	var namkot=document.getElementById("form1").select1.value;
+
+	if(namkot=="Aceh"){
+		alert("Mie Aceh");
+	}
+
+	else if(namkot=="Sumatera Utara"){
+		alert("Bika Ambon");
+	}
+
+	else if(namkot=="Maluku"){
+		alert("Ikan Asar");
+	}
+
+	else if(namkot=="Banten"){
+		alert("Sate Bandeng");
+	}
+	else if(namkot=="Bali"){
+		alert("Ayam Betutu");
+	}
+}
